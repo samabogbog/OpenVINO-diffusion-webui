a2b0426eb3b6793a5a811ec4ab925475690c8ae0
Khunrambo <95111793+samabogbog@users.noreply.github.com>
2025-06-09 08:46:49 +0700

    Update webui.py

--- webui.py
+++ webui.py
@@ -1,7 +1,7 @@
 from gradio_ui import create_gradio_interface
 
 if __name__ == "__main__":
-    print("Starting NinjaGenAI Web UI...")
+    print("Starting OpenVINO Diffusion Web UI...")
     demo = create_gradio_interface()
     
     demo.launch(
